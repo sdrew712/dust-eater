e3800b1564d4a181ac6ed56a7dcba9fba72de9e7
Sarah Drew <drew.sarah.e@gmail.com>
2021-11-11 15:25:15 -0700

    update styles

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import axios from "axios";
+require("dotenv").config();
+
+const URL = process.env.REACT_APP_URL;
+
+const handleDirection = async (command) => {
+  try {
+    axios.post(URL, command);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+function App() {
+  return (
+    <>
+      <h1>Dust Eater</h1>
+
+      <button id="forward" onClick={({ target }) => handleDirection(target.innerHTML)}>
+        forward
+      </button>
+
+      <div className="buttons">
+        <button id="left" onClick={({ target }) => handleDirection(target.innerHTML)}>
+          left
+        </button>
+        <button id="stop" onClick={({ target }) => handleDirection(target.innerHTML)}>
+          stop
+        </button>
+        <button id="right" onClick={({ target }) => handleDirection(target.innerHTML)}>
+          right
+        </button>
+      </div>
+
+      <button id="back" onClick={({ target }) => handleDirection(target.innerHTML)}>
+        back
+      </button>
+    </>
+  );
+}
+
+export default App;
